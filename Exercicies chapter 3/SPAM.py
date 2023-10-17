c42e24b9a826d04fdbdbc9efab4d1c1ae44fa7ce
Vinícius Passos <viniciusribeiro86@hotmail.com>
2023-10-17 14:20:57 -0300

    Adicionado o arquivo SPAM.py para carregar os dadaos a serem analisados.

---
+++ Exercicies chapter 3/SPAM.py
@@ -0,0 +1,43 @@
+import os
+import tarfile
+import urllib.request
+
+DOWNLOAD_ROOT = "http://spamassassin.apache.org/old/publiccorpus/"
+HAM_URL = DOWNLOAD_ROOT + "20030228_easy_ham.tar.bz2"
+SPAM_URL = DOWNLOAD_ROOT + "20030228_spam.tar.bz2"
+SPAM_PATH = os.path.join("datasets", "spam")
+
+def fetch_spam_data(ham_url=HAM_URL, spam_url=SPAM_URL, spam_path=SPAM_PATH):
+    if not os.path.isdir(spam_path):
+        os.makedirs(spam_path)
+    for filename, url in (("ham.tar.bz2", ham_url), ("spam.tar.bz2", spam_url)):
+        path = os.path.join(spam_path, filename)
+        if not os.path.isfile(path):
+            urllib.request.urlretrieve(url, path)
+        tar_bz2_file = tarfile.open(path)
+        tar_bz2_file.extractall(path=spam_path)
+        tar_bz2_file.close()
+
+fetch_spam_data()
+
+HAM_DIR = os.path.join(SPAM_PATH, "easy_ham")
+SPAM_DIR = os.path.join(SPAM_PATH, "spam")
+ham_filenames = [name for name in sorted(os.listdir(HAM_DIR)) if len(name) > 20]
+spam_filenames = [name for name in sorted(os.listdir(SPAM_DIR)) if len(name) > 20]
+
+import email
+import email.policy
+
+def load_email(is_spam, filename, spam_path=SPAM_PATH):
+    directory = "spam" if is_spam else "easy_ham"
+    with open(os.path.join(spam_path, directory, filename), "rb") as f:
+        return email.parser.BytesParser(policy=email.policy.default).parse(f)
+    
+ham_emails = [load_email(is_spam=False, filename=name) for name in ham_filenames]
+spam_emails = [load_email(is_spam=True, filename=name) for name in spam_filenames]
+
+print(ham_emails[1].get_content().strip())
+
+# %%
+print(spam_emails[6].get_content().strip())
+# %%
