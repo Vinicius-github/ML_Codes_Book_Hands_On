c1e40c763c86a42a484c939bdada84598eecc6be
Vinícius Passos <viniciusribeiro86@hotmail.com>
2023-11-30 14:24:10 -0300

    Added graphic to compare values between predicted values and original values

--- Exercicies chapter 4/california_house_svm.py
+++ Exercicies chapter 4/california_house_svm.py
@@ -8,6 +8,7 @@ from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
 from sklearn.svm import LinearSVR, SVR
 from sklearn.metrics import mean_squared_error
+import matplotlib.pyplot as plt
 
 #Fetching dataset
 housing = fetch_california_housing()
@@ -41,3 +42,20 @@ y_pred_test = svr.predict(X_test_scaled)
 mse_test = mean_squared_error(y_test, y_pred_test)
 print("The mse value for train dataset is: ", mse_test)
 print("The RMSE value for train dataset is: ", np.sqrt(mse_test))
+
+#Ploty the top 10 values train dataset
+plt.plot(y_train[:10], color = "blue")
+plt.plot(y_pred[:10], color = "red")
+plt.title('SVR - Top ten values (Train dataset)')
+plt.xlabel('Position level')
+plt.ylabel('Housing value')
+plt.show()
+
+#Ploty the top 10 values train dataset
+plt.plot(y_test[:10], color = "blue")
+plt.plot(y_pred_test[:10], color = "red")
+plt.title('SVR - Top ten values (Test dataset)')
+plt.xlabel('Position level')
+plt.ylabel('Housing value')
+plt.show()
+
