36c3dd2ae6f20695c299be02d198b89314c5649c
Vinícius Passos <viniciusribeiro86@hotmail.com>
2023-11-29 21:32:17 -0300

    The dataset has been modified

--- Exercicies chapter 4/california_house_svm.py
+++ Exercicies chapter 4/california_house_svm.py
@@ -1,71 +1,43 @@
-#import libraries
 import pandas as pd
 import numpy as np
-from sklearn.metrics import mean_squared_error
+from sklearn.datasets import fetch_california_housing
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
-from sklearn.pipeline import Pipeline
+from sklearn.pipeline import make_pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.impute import SimpleImputer
-from sklearn.svm import SVR, LinearSVR
+from sklearn.svm import LinearSVR, SVR
+from sklearn.metrics import mean_squared_error
 
-#load dataset
-url = "https://raw.githubusercontent.com/ageron/handson-ml2/master/datasets/housing/housing.csv"
-dataset = pd.read_csv(url)
+#Fetching dataset
+housing = fetch_california_housing()
+X = housing["data"]
+y = housing["target"]
 
-#Split dataset with Stratified
-dataset['income_cat'] = pd.cut(dataset["median_income"],
-        bins = [0.,1.5,3.0,4.5,6.,np.inf])
-split = StratifiedShuffleSplit(n_splits=1, test_size=0.2, random_state=42)
-for train_index, test_index in split.split(dataset, dataset["income_cat"]):
-    strat_train_set=dataset.loc[train_index]
-    strat_test_set=dataset.loc[test_index]
-for set_ in (strat_train_set, strat_test_set):
-    set_.drop("income_cat",axis=1,inplace=True)
+#Split daaset
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-print(strat_train_set.size)
-print(strat_test_set.size)
+#Transform
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
 
-#Model pipeline
+#Train
+svr = SVR()
+svr.fit(X_train_scaled, y_train)
 
-#Train data
-housing = strat_train_set.drop("median_house_value", axis=1)
-housing_labels = strat_train_set["median_house_value"].copy()
+#Predict
+y_pred = svr.predict(X_train_scaled)
 
-#Test data
-housing_test = strat_test_set.drop("median_house_value", axis=1)
-housing_labels_test = strat_test_set["median_house_value"].copy()
+#Results train
+mse = mean_squared_error(y_train, y_pred)
+print("The mse value for train dataset is: ", mse)
+print("The RMSE value for train dataset is: ", np.sqrt(mse))
 
-#Transforms
-num_preprocess = Pipeline([
-    ('imputer', SimpleImputer(strategy='median')),
-    ('scaler', StandardScaler())
-])
+#Predict
+y_pred_test = svr.predict(X_test_scaled)
 
-cat_preprocess = Pipeline([
-    ('onehot', OneHotEncoder(handle_unknown='ignore'))
-])
-
-pipe_preprocess = ColumnTransformer([
-    ('num',num_preprocess,list(housing.select_dtypes(include="float64"))),
-    ('cat',cat_preprocess,list(housing.select_dtypes(include="object")))
-])
-
-model_svr = Pipeline([
-    ("preprocess", pipe_preprocess),
-    ('svr', SVR())
-])
-
-model_linear_svr = Pipeline([
-    ("preprocess", pipe_preprocess),
-    ('linear_svr', LinearSVR())
-])
-
-model_svr.fit(housing, housing_labels)
-model_linear_svr.fit(housing, housing_labels)
-
-y_pred_train = model_svr.predict(housing)
-mse_train = mean_squared_error(housing_labels, y_pred_train)
-print(mse_train)
-print(np.sqrt(mse_train))
+#Results test
+mse_test = mean_squared_error(y_test, y_pred_test)
+print("The mse value for train dataset is: ", mse_test)
+print("The RMSE value for train dataset is: ", np.sqrt(mse_test))
